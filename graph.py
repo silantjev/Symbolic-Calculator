4f873568df03054de1c97fbed549900dd9a9d535
silant <blacks@mail.ru>
2023-04-02 16:31:56 +0300

    insert_abs in graph.py is corrected

--- graph.py
+++ graph.py
@@ -1,7 +1,41 @@
-# This module contains functions to parse the input expression and make the calculation graph.
+# This module contains functions that clean the input expression
+# parse it and to make the calculation graph.
+
+
+# The two main functions:
+
+
+def cleaning(expr):
+    """ Checks and clean the expression 'expr' """
+    assert is_balanced(expr)
+    expr = expr.strip()
+    replace_dict = {
+        '[': '(', '{': '(', ']': ')', '}': ')',
+        ' mod': '%',
+        '\\mod': '%',
+        #',': '.',
+        '**': '^'
+        }
+    expr = replace_all(expr, replace_dict)
+    expr = insert_abs(expr)
+    return delete_all(expr, '$\\ \t\n')
+
+
+def make_graph(expr: str) -> dict:
+    """ Creates new calculation graph for the expression 'expr' """
+    graph = {}
+    insert(graph, expr)
+    return graph
+
+
+
+# The others functions are auxiliary:
+
 
 def is_balanced(expr):
-    """ Checks the correctness of the bracket positions """
+    """ Checks the correctness of the bracket positions.
+    Checks also modulus symbols '|'.
+    """
     stack = []
     brackets = {'(': ')', '[': ']', '{': '}'}
     for symbol in expr:
@@ -41,46 +75,40 @@ def delete_all(expr, del_list):
 
 
 def insert_abs(expr):
-    stack = []
+    """ Replaces '|some_expression|' in the string 'expr' by 'abs(some_expression)'.
+    The expression 'expr' is supposed to be checked by the function 'is_balanced'.
+    The expression 'expr' is supposed to be cleaned from spaces etc.
+    Realised by loop instead of recursion.
+    """
+    
+    unacceptable = '+-/*^%' # symbols that can not be in the end of argument of modulus
+
     while True:
-        i = expr.find('|')
-        if i == -1:
+        i = expr.find('|') # index of the first '|'
+        if i == -1: # if not found
             break
-        else:
-            j = -1-expr[::-1].find('|') # index of the last '|'
+
+        j = i + 1 +expr[i+1:].find('|') # index of the next '|'
+
+        while True:
             if j == -1:
-                after = ''
-            else:
-                after = expr[j+1:]
-            expr = expr[:i] + 'abs(' + expr[i+1:j] + ')' + after
+                raise AssertionError('The modulus symbols "|" are not in balance')
+            if expr[j-1] not in unacceptable:
+                expr = expr[:i] + 'abs(' + expr[i+1:j] + ')' + expr[j+1:]
+                break
+
+            # if the symbol before the considered '|' is unacceptable,
+            # then it should be nested modulus, so we check the next one:
+
+            j = j + 1 + expr[j+1:].find('|') # index of the next '|'
 
     return expr
 
 
-def cleaning(expr):
-    """ Checks and cleans the expression """
-    assert is_balanced(expr)
-    expr = expr.strip()
-    replace_dict = {
-        '[': '(', '{': '(', ']': ')', '}': ')',
-        ' mod': '%',
-        '\\mod': '%',
-        #',': '.',
-        '**': '^'
-        }
-    expr = replace_all(expr, replace_dict)
-    expr = insert_abs(expr)
-    return delete_all(expr, '$\\ \t\n')
-
-def make_graph(expr: str) -> dict:
-    """ Creates new calculation graph for the expression 'expr' """
-    graph = {}
-    insert(graph, expr)
-    return graph
-
-
 def insert(graph: dict, expr: str):
-    """ Inserts the expression 'expr' to the graph 'graph' """
+    """ Add the parsed expression 'expr' to the dictionary 'graph'.
+    Finally, recurrently does the same for all arguments.
+    """
     expr0 = expr
     while True:
         if expr.startswith('(') and expr.endswith(')'):
@@ -132,24 +160,34 @@ def parse(expr: str) -> tuple:
 
 
 def splitting2(expr, operations):
-    """ Searches first 'operation' in the top level parts of the expression 'expr' """
-    """ Returns index and operation to split the expression it into 2 parts """
-    stack = 0
-    end = -1
+    """ Searches first operation from 'operations'
+    in the top level parts of the expression 'expr'
+    ("level" means the depth of the nested parentheses),
+    returns index and operation to split the expression into 2 parts.
+
+    Note that the operations are searched in the order of 'operations',
+    not by their positions in the expression;
+    this gives a little different graph,
+    but it does not affect the final result.
+    """
+
+    level = 0 # top level
+    # The variable 'end' means the index of the previous ')'
+    end = -1 # The value -1 means that there was not ')' before
     for i, s in enumerate(expr):
         if s == '(':
-            if stack == 0:
-                # check the expression before all '(' (if end=-1) and between ')', '(':
+            if level == 0:
+                # Check the expression before all '(' (if end=-1) and between ')', '(':
                 expr_to_check = expr[end+1:i]
                 for oper in operations:
                     j = expr_to_check.find(oper)
                     if j != -1:
                         return end+1+j, oper
-            stack += 1
+            level += 1
         elif s == ')':
             end = i
-            stack -= 1
-    # finally check the expression after all ')':
+            level -= 1
+    # Finally, check the expression after all ')':
     for oper in operations:
         expr_to_check = expr[end+1:]
         j = expr_to_check.find(oper)
@@ -158,10 +196,10 @@ def splitting2(expr, operations):
 
 
 def splitting_comma(expr):
-    """ Splits the expression 'expr' by commas on the top level parts"""
+    """ Splits the expression 'expr' by commas on the top level parts """
     args =[]
     sp = splitting2(expr, ',')
-    i = -1 # needed for the case sp == None
+
     while sp:
         i, oper = sp
         arg = expr[:i]
@@ -169,16 +207,21 @@ def splitting_comma(expr):
         expr = expr[i+1:]
         sp = splitting2(expr, ',')
 
+    # If 'sp' becomes None, then there is no more comma,
+    # so the rest 'expr' is the last argument:
     args.append(expr)
     return args
 
 
 if __name__ == '__main__':
-    expr = 'abs(x+4) + y!'
+    # Examples:
+    expr = '|x+|z|-y|'
+    expr = '|x+z|+|y|'
     expr = cleaning(expr)
-    print(expr)
+    print('Expression:', expr)
+    print('Calculation graph:')
     graph = make_graph(expr)
     for k, v in graph.items():
-        print(f'\'{k}\': {v}')
+        print(f' \'{k}\': {v}')
 
 
