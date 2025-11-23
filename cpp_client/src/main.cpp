db3dd8e7ef92dae69395277323811fd56f5492e0
Alexey Silantyev <blacks@mail.ru>
2025-11-23 20:16:53 +0300

    cpp_calc_clieant + log

--- cpp_client/src/main.cpp
+++ cpp_client/src/main.cpp
@@ -3,9 +3,19 @@
 #include <QDir>
 #include <QMainWindow>
 
+#include <log.h>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    Logger logger(true);
+    static Logger* loggerPtr = &logger;
+    qInstallMessageHandler([](auto... args) { loggerPtr->handle(args...); });
+    qDebug() << "Debug string";
+    qInfo() << "Info string";
+    qWarning() << "Warning string";
+    qCritical() << "Critical string";
 
     //Устанавливаем путь к плагинам относительно бинарника
     // QDir binDir(QCoreApplication::applicationDirPath());
